12d541f5c101e985b84b6384761a5b483e70fd23
yamnang <sonamd5dorji@gmail.com>
2025-08-19 18:25:50 +0600

    fix 1

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,15 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
  
-const config = getDefaultConfig(__dirname)
- 
-module.exports = withNativeWind(config, { input: './global.css' })
+const config = getDefaultConfig(__dirname);
+
+// Add Reanimated support to prevent crashes
+config.transformer = {
+  ...config.transformer,
+  minifierConfig: {
+    keep_fnames: true, // Prevents function name mangling that breaks Reanimated
+  },
+};
+
+// Apply NativeWind
+module.exports = withNativeWind(config, { input: './global.css' });
